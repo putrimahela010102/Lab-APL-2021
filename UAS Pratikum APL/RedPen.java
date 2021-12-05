e2d021ad590e42dfce7f09fe3f43e23721afabf0
Putri mahela <72485012+putrimahela010102@users.noreply.github.com>
2021-12-05 10:19:14 +0700

    Update RedPen.java

--- UAS Pratikum APL/RedPen.java
+++ UAS Pratikum APL/RedPen.java
@@ -3,11 +3,11 @@ public class RedPen implements Pen
     @Override
     public void color()
     {
-        System.out.println("merah");
+        System.out.println("merah"); //menampilkan warna dari pena
     }
 
     public double getPanjang()
     {
-        return 10.0;
+        return 10.0; //menentukan panjang pena yang akan di return
     }
 }
