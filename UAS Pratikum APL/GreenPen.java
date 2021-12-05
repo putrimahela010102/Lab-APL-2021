61a8f390beb5e899259366dfc3b831e2c4cd41d3
Putri mahela <72485012+putrimahela010102@users.noreply.github.com>
2021-12-05 10:21:01 +0700

    Update GreenPen.java

--- UAS Pratikum APL/GreenPen.java
+++ UAS Pratikum APL/GreenPen.java
@@ -3,11 +3,11 @@ public class GreenPen implements Pen
     @Override
     public void color()
     {
-        System.out.println("Pena hijau");
+        System.out.println("Pena hijau"); //menampilkan warna dari pena
     }
 
     public double getPanjang()
     {
-        return 7.0;
+        return 7.0; //menentukan panjang pena yang akan di return
     }
 }
